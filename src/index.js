5b0054d2b540387ed05a15da7c5eedc8ed9af9f6
Carlos Lott <celotts@gmail.com>
2021-11-16 15:13:18 -0300

    feat(cambios): se agregan mejoras

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-'use strict'
+"use strict";
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-
 const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: true})); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/hola', (req, res) => { res.send({message:'Hola Mundo Lott'}) });
+app.get("/hola", (req, res) => {
+  res.send({ message: "Hola Mundo Lott" });
+});
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
