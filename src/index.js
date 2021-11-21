7b729926b21c1216619ac4345acc726a8bb7a6c2
Carlos Lott <celotts@gmail.com>
2021-11-21 15:16:07 -0300

    feat(prueba): es una prueba

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.get("/hola", (req, res) => {
   res.send({ message: "Hola Mundo Lott" });
 });
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  //console.log(`Server is running on port ${port}`);
 });
